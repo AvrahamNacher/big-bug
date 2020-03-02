86955a28992d63eb5a0e4e356c4e988fb18fea00
Avraham Nacher <nacher613@gmail.com>
2020-03-02 16:58:10 +0200

    master checkbox to select for bug deletion

--- src/components/BugListView.jsx
+++ src/components/BugListView.jsx
@@ -1,16 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import BugListViewHeader from './BugListViewHeader';
+import * as dbBugs from '../backend/dbBugRequests';
 
 import './BugListView.css'
 
-export default function BugListView (props) {
-    function listBugs () {
+export default function BugListView(props) {
+    const [bugsToDelete, setBugsToDelete] = useState([]);
 
-        // debugger;
-        
-        return props.bugList.map( bug => {
-            const { 
+    const toggleCheckbox = e => {
+        const { name, checked } = e.target; // destructuring
+        if (checked) {
+            setBugsToDelete([...bugsToDelete, name])
+        } else {
+            setBugsToDelete(bugsToDelete.filter(el => el !== name));
+        };
+    }
+
+    const toggleAllCheckBoxes = e => {
+        const { checked : masterCheckbox } = e.target;
+        console.log("master checkbox = " + masterCheckbox);
+        if (masterCheckbox) {
+            let checkAllItems =props.bugList.map ( el => `(id=${el.id})`);
+            setBugsToDelete(checkAllItems);
+            
+        } else {
+            setBugsToDelete([]);
+        }
+    }
+
+    const deleteCallback = response => {
+        console.log(response);
+    }
+
+    const deleteBugs = () => {
+        let SQL = bugsToDelete.join(' OR ');
+        console.log(SQL);
+        dbBugs.deleteBugs(SQL, deleteCallback);
+    }
+
+    function listBugs() {
+
+        return props.bugList.map(bug => {
+            const {
                 id,
                 bugTitle,
                 // bugDescription,
@@ -21,35 +53,36 @@ export default function BugListView (props) {
                 bugStatus,
                 bugSeverity,
                 bugReproducableFrequency
-                } = bug; //destructuring
+            } = bug; //destructuring
+            // setBugsToDelete()
 
-                return (
-                    <tr key={id}>
-                        <td><input className="bug_list_view_item bug_list_view_check_box" type="checkbox" name="mainCheckAll"></input></td>
-                        <td className = "bug_list_view_item bug_list_view_bug_id"><Link to={`/bug/${id}`}>{id}</Link></td>
-                        <td className = "bug_list_view_item bug_list_view_bug_title"><Link to={`/bug/${id}`}>{bugTitle}</Link></td>
-                        {/* <td className = "bug_list_view_item bug_list_view_bug_description">{bugDescription}</td> */}
-                        <td className = "bug_list_view_item bug_list_view_bug_createdDate"><Link to={`/bug/${id}`}>{bugCreatedDate}</Link></td>
-                        {/* <td className = "bug_list_view_item bug_list_view_bug_createdBy">{bugCreatedBy}</td> */}
-                        <td className = "bug_list_view_item bug_list_view_bug_assignedTo"><Link to={`/bug/${id}`}>{bugAssignedTo}</Link></td>
-                        <td className = "bug_list_view_item bug_list_view_bug_dueDate"><Link to={`/bug/${id}`}>{bugDueDate}</Link></td>
-                        <td className = "bug_list_view_item bug_list_view_bug_status"><Link to={`/bug/${id}`}>{bugStatus}</Link></td>
-                        <td className = "bug_list_view_item bug_list_view_bug_severity"><Link to={`/bug/${id}`}>{bugSeverity}</Link></td>
-                        <td className = "bug_list_view_item bug_list_view_bug_reproducibleFrequency"><Link to={`/bug/${id}`}>{bugReproducableFrequency}</Link></td>
-                    </tr>
-                )}
-            );
+            return (
+                <tr key={id}>
+                    <td><input className="bug_list_view_item bug_list_view_check_box" type="checkbox" name={`(id=${id})`} onChange={toggleCheckbox} checked={bugsToDelete.includes(`(id=${id})`)}></input></td>
+                    <td className="bug_list_view_item bug_list_view_bug_id"><Link to={`/bug/${id}`}>{id}</Link></td>
+                    <td className="bug_list_view_item bug_list_view_bug_title"><Link to={`/bug/${id}`}>{bugTitle}</Link></td>
+                    {/* <td className = "bug_list_view_item bug_list_view_bug_description">{bugDescription}</td> */}
+                    <td className="bug_list_view_item bug_list_view_bug_createdDate"><Link to={`/bug/${id}`}>{bugCreatedDate}</Link></td>
+                    {/* <td className = "bug_list_view_item bug_list_view_bug_createdBy">{bugCreatedBy}</td> */}
+                    <td className="bug_list_view_item bug_list_view_bug_assignedTo"><Link to={`/bug/${id}`}>{bugAssignedTo}</Link></td>
+                    <td className="bug_list_view_item bug_list_view_bug_dueDate"><Link to={`/bug/${id}`}>{bugDueDate}</Link></td>
+                    <td className="bug_list_view_item bug_list_view_bug_status"><Link to={`/bug/${id}`}>{bugStatus}</Link></td>
+                    <td className="bug_list_view_item bug_list_view_bug_severity"><Link to={`/bug/${id}`}>{bugSeverity}</Link></td>
+                    <td className="bug_list_view_item bug_list_view_bug_reproducibleFrequency"><Link to={`/bug/${id}`}>{bugReproducableFrequency}</Link></td>
+                </tr>
+            )
+        }
+        );
     }
     return (
         <div>
-
             <table id="bug_list_view_table">
                 <thead>
                     <tr key={0}>
-                        <th><input className="bug_list_view_item bug_list_view_check_box" type="checkbox" name="mainCheckAll"></input></th>
-                        <BugListViewHeader 
-                            headerInfo={ props.bugList[0]}  
-                            bugList={props.bugList} 
+                        <th><input className="bug_list_view_item bug_list_view_check_box" type="checkbox" name="mainCheckAll" onChange={toggleAllCheckBoxes} checked={bugsToDelete.length === props.bugList.length}></input></th>
+                        <BugListViewHeader
+                            headerInfo={props.bugList[0]}
+                            bugList={props.bugList}
                             setBugList={props.setBugList}
                         />
                     </tr>
@@ -58,7 +91,7 @@ export default function BugListView (props) {
                     {listBugs()}
                 </tbody>
             </table>
-
+            {bugsToDelete.length > 0 && <div><input onClick={deleteBugs} type="button" value="DELETE"></input></div>}
         </div>
     );
 }
