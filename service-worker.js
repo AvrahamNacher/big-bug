1f5bc9595e9dcb537a03ab62b69464a97532aafc
Avraham Nacher <nacher613@gmail.com>
2020-04-24 18:53:51 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/big-bug/precache-manifest.b8fa046a5f4ca260b07fce53ab4f9e2e.js"
+  "/big-bug/precache-manifest.9a6f64872cee9eae999215ac29d6bc08.js"
 );
 
 self.addEventListener('message', (event) => {
