f514b1aeaa94f10d86a8e52fd5fb8cd6c974e9f2
Avraham Nacher <nacher613@gmail.com>
2020-05-20 18:17:20 +0300

    header shows names instead of database keys

--- src/components/BugListViewHeader.jsx
+++ src/components/BugListViewHeader.jsx
@@ -7,45 +7,39 @@ let sortDirection = true;
 let previousSortColumnName = '';
 
 export default function BugListViewHeader(props) {
-    // console.log("in bugHeader", props.headerInfo);
-
-    // function renameKeys(obj, newKeys) {
-    //     const keyValues = Object.keys(obj).map(key => {
-    //         const newKey = newKeys[key] || key;
-    //         return { [newKey]: obj[key] };
-    //     });
-    //     return Object.assign({}, ...keyValues);
-    // }
-
-
-    const bugListViewKeys = ['id', 'bugTitle', 'bugCreatedDate', 'bugAssignedTo', 'bugDueDate', 'bugStatus', 'bugSeverity', 'bugReproducibility'];
-    // TODO update header names, but retain sort option with real key names
-    // const bugListViewKeysToDisplay = ['id', 'title', 'createdDate', 'assigned to', 'due', 'status', 'severity', 'reproducable'];
-    // debugger;
+   
+    const bugListViewKeys = [{
+        key: 'id', label: 'Id #'
+    }, {
+        key: 'bugTitle', label: 'Title'
+    }, {
+        key: 'bugCreatedDate', label: 'Created On'
+    }, {
+        key: 'bugAssignedTo', label: 'Assigned To'
+    }, {
+        key: 'bugDueDate', label: 'Due Date'
+    }, {
+        key: 'bugStatus', label: 'Status'
+    }, {
+        key: 'bugSeverity', label: 'Severity'
+    }, {
+        key: 'bugReproducibility', label: 'Reproducibility'
+    }];
     let allKeys = Object.keys(props.headerInfo);
-    let keysToShow = allKeys.filter( key => bugListViewKeys.includes(key));
+    let keysToShow = allKeys.filter(show => bugListViewKeys.map( keyName => keyName.key ).indexOf(show));
     keysToShow = bugListViewKeys;
-
-    // const newKeyNames = { bugTitle: "title", bugCreatedDate: "created"};
-    // keysToShow = renameKeys(keysToShow, newKeyNames);
-
-    // create initial sort based on first key in list
-    // useEffect( () => {
-    //     props.setBugList(sortColumn (props, bugListViewKeys[0]));
-    // },[]);
-
     
     return (
-        keysToShow.map( (key, index) => {
-            return <th onClick={() => { props.setBugList( sortColumn(props, key) ); } } 
+        keysToShow.map((key, index) => {
+            return <th onClick={() => { props.setBugList(sortColumn(props, key.key)); }}
                 key={index}>
-                {key.toUpperCase()}
+                {key.label.toUpperCase()}
             </th>
         })
     )
 }
 
-function sortStringColumn (a, b, sortColumnName) {
+function sortStringColumn(a, b, sortColumnName) {
     let comparison = 0;
     if (a[sortColumnName].toLowerCase() > b[sortColumnName].toLowerCase()) {
         comparison = (sortDirection) ? 1 : -1;
@@ -55,7 +49,7 @@ function sortStringColumn (a, b, sortColumnName) {
     return comparison;
 }
 
-function sortNumberColumn (a, b, sortColumnName) {
+function sortNumberColumn(a, b, sortColumnName) {
     let comparison = 0;
     if (a[sortColumnName] > b[sortColumnName]) {
         comparison = (sortDirection) ? 1 : -1;
@@ -65,7 +59,7 @@ function sortNumberColumn (a, b, sortColumnName) {
     return comparison;
 }
 
-function sortColumn (props, sortColumnName) {
+function sortColumn(props, sortColumnName) {
     if (previousSortColumnName === sortColumnName) {
         sortDirection = !sortDirection;
     } else {
@@ -78,10 +72,10 @@ function sortColumn (props, sortColumnName) {
 
     switch (dataType) {
         case 'string':
-            newSort.sort( (a, b) => sortStringColumn (a, b, sortColumnName));
+            newSort.sort((a, b) => sortStringColumn(a, b, sortColumnName));
             break;
         case 'number':
-            newSort.sort( (a, b) => sortNumberColumn (a, b, sortColumnName));
+            newSort.sort((a, b) => sortNumberColumn(a, b, sortColumnName));
             break;
         default:
             break;
